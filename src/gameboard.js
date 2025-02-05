82d7e8df796beafc6bb27ce3442dad1ddac47657
carlDguez19 <carldominguez19@gmail.com>
2025-02-04 18:01:19 -0700

    added the receive attack method which will determine if the attack hits an empty or previously missed spot, if it does then it misses again. If the cell contains a ship then we look for that ship in the array of ships and call the .hit().

--- src/gameboard.js
+++ src/gameboard.js
@@ -19,11 +19,23 @@ export class Gameboard{
             this.ships.append(testShip);
             if(axis == "y"){//if ship is placed vertically then fill the cells it will take up with the length
                 for(let i = 0; i < length; i++){
-                    board[coords[0]+i][coords[1]] = length;
+                    board[coords[0]+i][coords[1]] = testShip;
                 }
-            }else{
+            }else{//ship placed horizontally
                 for(let i = 0; i < length; i++){
-                    board[coords[0]][coords[1]+1] = length;
+                    board[coords[0]][coords[1]+1] = testShip;
+                }
+            }
+            return board;//maybe not needed
+        }
+    }
+    receiveAttack(coords, board){
+        if(board[coords[0]][coords[1]] == undefined || board[coords[0]][coords[1]] == "0"){//if cell on 2d array is empty or was previously a miss then declare a miss
+            board[coords[0]][coords[1]] == "0";
+        }else{
+            for(let i = 0; i < this.ships.length; i++){//if cell has a ship then look for that ship in array of ships and .hit()
+                if(board[coords[0]][coords[1]] == this.ships[i]){
+                    this.ships[i].hit();
                 }
             }
         }
