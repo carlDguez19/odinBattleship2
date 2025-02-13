35fd318bb0ec80f43737e22df62b5efe08f88bac
carlDguez19 <carldominguez19@gmail.com>
2025-02-13 16:55:25 -0700

    I removed the chooseLocation method from the player class. I feel that its a bit redundant its almost the same as placeShip method which has already been tested. The logic determining where the ships are placed based on type of player goes somewhere else i feel.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import { Ship } from "./ship";
 import { Gameboard } from "./gameboard";
 import { Player } from "./player";
 
-const gBoard5 = new Gameboard(5);
-gBoard5.placeShip(2,[1,1],"x", gBoard5.board);
-gBoard5.receiveAttack([1,1], gBoard5.board);
-const testShip = gBoard5.board[1][1];
-console.log(testShip);
+// const gBoard5 = new Gameboard(5);
+// gBoard5.placeShip(2,[1,1],"x", gBoard5.board);
+// gBoard5.receiveAttack([1,1], gBoard5.board);
+// const testShip = gBoard5.board[1][1];
+// console.log(testShip);
+
+const player1 = new Player("real", 5);
+const player2 = new Player("real", 5);
+
+player1.chooseLocation(3,[2,2],"y");
+player1.chooseLocation(2,[0,1],"x");
+player1.chooseLocation(2,[2,0],"x");
+
+player2.chooseLocation(3,[1,2],"x");
+player2.chooseLocation(2,[2,1],"y");
+player2.chooseLocation(2,[3,3],"y");
--- src/player.js
+++ src/player.js
@@ -3,23 +3,23 @@ import { Gameboard } from "./gameboard";
 export class Player{
     constructor(type, length){
         this.type = type;
-        this.board = new Gameboard(length);
+        this.pBoard = new Gameboard(length);
     }
 
     //player should choose location of ships and gameboard should placeShip
-    chooseLocation(type, length, coords, axis){
-        if(type == "real"){
-            this.board.placeShip(length, coords, axis, this.board.board);
-        }else{
-            //placeShip at random locations if player is a computer player
-            let row = this.getRndInteger(0, this.board.board.length);
-            let col = this.getRndInteger(0, this.board.board.length);
-            let randAxis = this.getRndInteger(0, 2);
-            this.board.placeShip(length, [row,col], randAxis, this.board.board);
-        }
-    }
+    // chooseLocation(length, coords, axis){
+    //     if(this.type == "real"){
+    //         this.board.placeShip(length, coords, axis, this.board.board);
+    //     }else{
+    //         //placeShip at random locations if player is a computer player
+    //         let row = this.getRndInteger(0, this.board.board.length);
+    //         let col = this.getRndInteger(0, this.board.board.length);
+    //         let randAxis = this.getRndInteger(0, 2);
+    //         this.board.placeShip(length, [row,col], randAxis, this.board.board);
+    //     }
+    // }
 
-    getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
+    // getRndInteger(min, max) {
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
 }
